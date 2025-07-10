fe04189ce2f93deea6587c20e6190e7b2eb99072
maarselo <marce@decaos.com>
2025-07-10 13:32:42 +0200

    create first functions to exit, and to set signals on prompt mode

---
+++ src/signal.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signal.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbanzo-s <fbanzo-s@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/30 18:24:52 by fbanzo-s          #+#    #+#             */
+/*   Updated: 2025/06/30 18:55:47 by fbanzo-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static void	ft_sigint_handler(int signal)
+{
+	(void)signal;
+
+	write(STDOUT_FILENO, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
+
+void	ft_set_signal_prompt_mode(void)
+{
+	signal(SIGINT, ft_sigint_handler); 
+    signal(SIGQUIT, SIG_IGN);
+}
