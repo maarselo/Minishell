7c6ce7206b219912469571a6959af7b828a79b74
maarselo <marce@decaos.com>
2025-07-10 23:22:52 +0200

    REmoves functions to parser

--- src/tokenizer_utils.c
+++ src/tokenizer_utils.c
@@ -21,36 +21,3 @@ int	ft_is_operator(char c)
 {
 	return (c == '|' || c == '<' || c == '>' || c == '(' || c ==')');
 }
-
-/*
-int	ft_find_char(int c, char *input)
-{
-	int i = -1;
-	while (input[++i])
-	{
-		if (input[i] == 'c')
-			return (1);
-	}
-	return (0);
-}
-
-int	ft_check_quotes(char *input)
-{
-	int	i;
-
-	i = -1;
-	while(input[++i])
-	{
-		if (input[i] == '\'')
-		{
-			if (!ft_find_char('\'', input + i))
-				return (1);
-		}
-		else if(input[i] == '"')
-		{
-			if (!ft_find_char('"', input + i))
-				return (1);
-		}
-	}
-	return (0);
-}*/
