469a24eb7eb60ea27610b9911c2c52483d46d6e9
maarselo <marce@decaos.com>
2025-07-12 22:35:27 +0200

    Add first parser to detect syntax errors

--- src/parser.c
+++ src/parser.c
@@ -11,8 +11,24 @@
 /* ************************************************************************** */
 
 #include "minishell.h"
-
+//show on 2? add returns
+/*
+bool validate_tokens(t_token *token_list)
+{
+	while (token)
+	{
+		// aquí pones todas las reglas una por una
+		token = token->next;
+	}
+}*/
 void	ft_parser(t_token *token)
 {
-	printf("%d", ft_count_command(token));	
+	if (ft_check_start_end_types(token))
+		return (printf("minishell: syntax error near unexpected operator\n"));
+	if (ft_check_delimiters(token))
+		return (printf("minishell: syntax error unmatched delimiter\n"));
+	if (ft_check_redirects(token))
+		return (printf("minishell: syntax error expected word after redirection\n"));
+	
+	
 }
--- src/parser_utils.c
+++ src/parser_utils.c
@@ -25,3 +25,71 @@ int	ft_count_command(t_token *token_list)
 	}
 	return (count);
 }
+
+int	ft_check_start_end_types(t_token *token)// | || &&
+{
+	t_token	*tmp;
+	
+	tmp = token;
+	if (tmp->type == 1 || tmp->type == 6 || tmp->type == 7)//check the first was, pipe, ||, &&
+		return (1);
+	while (tmp->next)
+		tmp = tmp->next;
+	if (tmp->type == 1 || tmp->type == 6 || tmp->type == 7)//check the last command
+		return (1);
+	return (0);
+}
+
+static int	ft_check_content_delimiters(char *input)
+{
+	int	i;
+	char delimiter_type;
+
+	i = 0;
+	while (input[i])
+	{
+		if (input[i] == '\'' || input[i] == '"' || input[i] == '(')
+		{
+			if (input[i] == '(')
+				delimiter_type = ')';
+			else
+				delimiter_type = input[i];
+			i++;
+			while (input[i] && input[i] != delimiter_type)
+				i++;
+			if (input[i] != delimiter_type)
+				return (1); 
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	ft_check_delimiters(t_token *token) //'"(
+{
+	t_token	*tmp;
+
+	tmp = token;
+	while (tmp)
+	{
+		if (ft_check_content_delimiters(tmp->content))
+			return (1);
+		tmp = tmp->next;
+	}
+	return (0);
+}
+
+int	ft_check_redirects(t_token *token)
+{
+	t_token	*tmp;
+	
+	tmp = token;
+	while(tmp)
+	{
+		if (((tmp->type == 2 || tmp->type == 3 || tmp->type == 4
+				|| tmp->type == 5)) && (!tmp->next || tmp->next->type != 0))
+			return (1);
+		tmp = tmp->next;
+	}
+	return (0);
+}
