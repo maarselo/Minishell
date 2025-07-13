0d04749c137a1e4da4a0239ac31db95632b6e9ad
maarselo <marce@decaos.com>
2025-07-13 05:09:40 +0200

    Rename file and add more fucntions to check syntax

--- src/syntax.c
+++ src/syntax.c
@@ -21,14 +21,22 @@ bool validate_tokens(t_token *token_list)
 		token = token->next;
 	}
 }*/
-void	ft_parser(t_token *token)
+void	*ft_syntax(t_token *token)
 {
+
 	if (ft_check_start_end_types(token))
-		return (printf("minishell: syntax error near unexpected operator\n"));
-	if (ft_check_delimiters(token))
-		return (printf("minishell: syntax error unmatched delimiter\n"));
+		return (printf("minishell: syntax error near unexpected operator\n"), NULL);
+	if (ft_check_quotes(token))
+		return (printf("minishell: syntax error unmatched delimiter\n"), NULL);
+	if (ft_check_parenthesis(token))
+		return (printf("minishell: syntax error unmatched parenthesis.\n"), NULL);
 	if (ft_check_redirects(token))
-		return (printf("minishell: syntax error expected word after redirection\n"));
-	
-	
+		return (printf("minishell: syntax error expected word after redirection\n"), NULL);
+	if (ft_check_between_operator(token))
+		return (printf("minishell: syntax error invalid combination or sequence of operators\n"), NULL);
+	if (ft_check_open_parenthesis(token))
+		return (printf("minishell: syntax error invalid opening parenthesis placement\n"), NULL);
+	if (ft_check_close_parenthesis(token))
+		return (printf("minishell: syntax error invalid closing parenthesis placement\n"), NULL);
+	return (NULL);
 }
