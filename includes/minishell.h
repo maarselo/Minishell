5d522299582aa8517e986bcc83568c2dfe5cc700
maarselo <marce@decaos.com>
2025-08-11 15:16:44 +0200

    Arrange header to put a mini introducction for any function and struct/enum , add the struct to the parser command, and finally create the global struct to the exit_code and enum to manipulate the different exit_codes

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fbanzo-s <fbanzo-s@student.42barcelona.    +#+  +:+       +#+        */
+/*   By: mvillavi <mvillavi@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 18:23:02 by fbanzo-s          #+#    #+#             */
-/*   Updated: 2025/07/12 23:06:14 by fbanzo-s         ###   ########.fr       */
+/*   Updated: 2025/08/11 15:04:53 by mvillavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,38 @@
 # include <stdbool.h>
 # include <signal.h>
 
+/*
+	minishell.h
+	Extern global to manipulate exit status from the last command
+*/
+
+typedef enum	exit_codes
+{
+	T_SUCCESS = 0,
+	T_GENERAL_ERROR = 1,
+	T_NON_EXISTENT_FILE = 1,
+	T_FILE_PERMITS = 1,
+	T_SYNTAX = 2,
+	T_NON_EXISTENT_DIRECTORY = 2,
+	T_DIRECTORY_PERMITS = 2,
+	T_EXECUTABLE_PERMITS = 126,
+	T_COMMAND_NOT_FOUND = 127,
+	T_SIGINT = 130,
+}			t_exit_types;
+
+typedef struct	s_global
+{
+	int exit_status;
+}				t_global;
+
+extern t_global	g_status;
+
+
+/*
+	tokenizer.h
+	Enum to diff the different types in the token_list and struct
+	to five the token list in linked list to parser.
+*/
 typedef enum e_token_type
 {
 	T_WORD,
@@ -34,14 +66,6 @@ typedef enum e_token_type
 	T_PAREN_CLOSE,
 }			t_token_type;
 
-typedef enum e_node_type
-{
-	NODE_CMD;
-	NODE_AND;
-	
-
-}			t_node_type;
-
 typedef struct s_token
 {
 	char			*content;
@@ -49,6 +73,22 @@ typedef struct s_token
 	bool			is_quoted;
 	struct s_token	*next;
 }	t_token;
+
+
+/*
+	parser.h
+	Enum to separate the differents commands and know if its necesarry 
+	pipes or only do. And struct to give to the expansor/ executor to 
+	execute more easy.
+*/
+typedef enum e_node_type
+{
+	NODE_CMD,
+	NODE_TYPE,
+	NODE_AND,
+	NODE_OR,
+
+}			t_node_type;
 
 typedef struct s_redirect
 {
@@ -61,7 +101,9 @@ typedef struct s_redirect
 
 typedef struct s_command
 {
-
+	char		**command;
+	t_node_type	type;
+	t_redirect	*redirection;
 }			t_commands;
 
 
@@ -71,15 +113,18 @@ void		ft_print_banner(void);
 // signals.c
 void		ft_set_signal_prompt_mode(void);
 
+//global.c
+int			ft_set_global_exit_status(int new_exit_code);
+
 //minishell.c
 void		ft_input_loop(char **envp);//to print all time minshell and get the input
 
-// token.c
-t_token		*ft_init_token(char *content);
 // tokenizer_utils.c
 int			ft_is_quote(char c);
 int			ft_is_operator(char c);
 char 		*ft_extract_token(char *input, int *i);
+// token.c
+t_token		*ft_init_token(char *content);
 // tokenizer.c
 void		ft_print_tokens(t_token	*token);
 t_token		*ft_tokenizer(char *input);
@@ -94,10 +139,15 @@ int			ft_check_redirects(t_token *token);
 int			ft_check_between_operator(t_token *token);
 int			ft_check_open_parenthesis(t_token *token);
 int			ft_check_close_parenthesis(t_token *token);
-
+int			ft_check_if_have_parenthesis(t_token *token);
 
 // syntax.c
-void	*ft_syntax(t_token *token);
+bool	ft_syntax(t_token *token);
+
+
+//parser.c
+
+
 
 // parser.c
 //int		ft_count_command(t_token *token_list);
@@ -105,6 +155,10 @@ void	*ft_syntax(t_token *token);
 
 // exit.c
 void		ft_exit_free_prompt(char *input);
+
+//free.c
+void    ft_free_token_and_input(char *input, t_token *token_list);
+
 
 
 #endif
