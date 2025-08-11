56956f12fdb429f54571143613217d0d5e473bba
maarselo <marce@decaos.com>
2025-08-11 15:11:30 +0200

    Modified to the main function of the syntax return a bool instead int

--- src/syntax.c
+++ src/syntax.c
@@ -11,34 +11,24 @@
 /* ************************************************************************** */
 
 #include "minishell.h"
-//show on 2? add returns
-/*
-bool validate_tokens(t_token *token_list)
-{
-	while (token)
-	{
-		// aquí pones todas las reglas una por una
-		token = token->next;
-	}
-}*/
-int	ft_syntax(t_token *token)
-{
 
+bool	ft_syntax(t_token *token)
+{
 	if (ft_check_start_end_types(token))
-		return (printf("minishell: syntax error near unexpected operator\n"), 1);
+		return (printf("minishell: syntax error near unexpected operator\n"), true);
 	if (ft_check_quotes(token))
-		return (printf("minishell: syntax error unmatched delimiter\n"), 1);
+		return (printf("minishell: syntax error unmatched delimiter\n"), true);
 	if (ft_check_parenthesis(token))
-		return (printf("minishell: syntax error unmatched parenthesis.\n"), 1);
+		return (printf("minishell: syntax error unmatched parenthesis.\n"), true);
 	if (ft_check_redirects(token))
-		return (printf("minishell: syntax error expected word after redirection\n"), 1);
+		return (printf("minishell: syntax error expected word after redirection\n"), true);
 	if (ft_check_between_operator(token))
-		return (printf("minishell: syntax error invalid combination or sequence of operators\n"), 1);
+		return (printf("minishell: syntax error invalid combination or sequence of operators\n"), true);
 	if (ft_check_open_parenthesis(token))
-		return (printf("minishell: syntax error invalid opening parenthesis placement\n"), 1);
+		return (printf("minishell: syntax error invalid opening parenthesis placement\n"), true);
 	if (ft_check_close_parenthesis(token))
-		return (printf("minishell: syntax error invalid closing parenthesis placement\n"), 1);
+		return (printf("minishell: syntax error invalid closing parenthesis placement\n"), true);
 	if (ft_check_if_have_parenthesis(token))
-		return (printf("minishell: parenthesis must be implemented in the future\n"), 1) ;
-	return (0);
+		return (printf("minishell: parenthesis must be implemented in the future\n"), true) ;
+	return (false);
 }
