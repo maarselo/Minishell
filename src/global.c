b3044bed2e1d3c94fc4c3be402c2d517549c2ce1
maarselo <marce@decaos.com>
2025-08-18 14:07:42 +0200

    Attach new file and functio to set global exit code to any exit code

--- src/global.c
+++ src/global.c
@@ -6,13 +6,13 @@
 /*   By: mvillavi <mvillavi@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 09:01:55 by mvillavi          #+#    #+#             */
-/*   Updated: 2025/08/11 15:08:35 by mvillavi         ###   ########.fr       */
+/*   Updated: 2025/08/13 18:13:10 by mvillavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	ft_set_global_exit_status(int new_exit_code)
+void	ft_set_global_exit_status(int new_exit_code)
 {
 	g_status.exit_status = new_exit_code;
 }
