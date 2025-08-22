ceed11615fd35dd842c631ea6552c290deec7317
maarselo <marce@decaos.com>
2025-08-22 18:33:34 +0200

    merge from main branch

--- src/syntax.c
+++ src/syntax.c
@@ -15,23 +15,6 @@
 bool	ft_syntax(t_token *token)
 {
 	if (ft_check_start_end_types(token))
-<<<<<<< HEAD
-		return (printf("minishell: syntax error near unexpected operator\n"), true);
-	if (ft_check_quotes(token))
-		return (printf("minishell: syntax error unmatched delimiter\n"), true);
-	if (ft_check_parenthesis(token))
-		return (printf("minishell: syntax error unmatched parenthesis.\n"), true);
-	if (ft_check_redirects(token))
-		return (printf("minishell: syntax error expected word after redirection\n"), true);
-	if (ft_check_between_operator(token))
-		return (printf("minishell: syntax error invalid combination or sequence of operators\n"), true);
-	if (ft_check_open_parenthesis(token))
-		return (printf("minishell: syntax error invalid opening parenthesis placement\n"), true);
-	if (ft_check_close_parenthesis(token))
-		return (printf("minishell: syntax error invalid closing parenthesis placement\n"), true);
-	if (ft_check_if_have_parenthesis(token))
-		return (printf("minishell: parenthesis must be implemented in the future\n"), true) ;
-=======
 		return (printf("minishell: syntax error near operator.\n"), true);
 	if (ft_check_quotes(token))
 		return (printf("minishell: unmatched quotes.\n"), true);
@@ -50,6 +33,5 @@ bool	ft_syntax(t_token *token)
 		printf("minishell: parenthesis must be implemented in the future\n");
 		return (true);
 	}
->>>>>>> main
 	return (false);
 }
