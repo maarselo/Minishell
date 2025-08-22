f84baf03e793af3af7522c40a89de87897a6747d
Francesc Banzo <12francescbs@gmail.com>
2025-08-22 16:10:02 +0200

    expander

--- src/expander_utils.c
+++ src/expander_utils.c
@@ -6,18 +6,38 @@
 /*   By: fbanzo-s <fbanzo-s@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 20:20:11 by fbanzo-s          #+#    #+#             */
-/*   Updated: 2025/08/22 13:34:57 by fbanzo-s         ###   ########.fr       */
+/*   Updated: 2025/08/22 15:33:45 by fbanzo-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+char	*ft_handle_expansion(char *result, char *str, int *i, int exit_status)
+{
+	char	*tmp;
+
+	if (str[*i] == '$')
+	{
+		tmp = ft_expand_var(str, i, exit_status);
+		result = ft_join_str_var(result, tmp);
+		free(tmp);
+	}
+	else
+	{
+		result = ft_join_char_var(result, str[*i]);
+		(*i)++;
+	}
+	return (result);
+}
 
 char	*ft_expand_tilde(char *result)
 {
 	char	*tmp;
 
 	tmp = getenv("HOME");
-	result = ft_join_str_var(result, tmp);
+	if (!tmp)
+		return (ft_strdup(result));
+	result = ft_join_str_var(result, tmp + 1);
 	return (result);
 }
 
