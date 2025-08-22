9bd0421527edd789c32c373aef270012f13634c6
maarselo <marce@decaos.com>
2025-08-22 14:24:49 +0200

    Add auxiliar functions from parser fill redirections command in the struct

---
+++ src/parser_redirection_utils.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser_redirection_utils.c                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvillavi <mvillavi@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/18 13:22:57 by mvillavi          #+#    #+#             */
+/*   Updated: 2025/08/18 14:01:24 by mvillavi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	ft_have_any_redirection(t_token *start, t_token *end)
+{
+	t_token	*tmp;
+
+	tmp = start;
+
+	while (tmp && tmp != end)
+	{
+		if (ft_get_if_its_redirection_type(tmp))
+			return (1);
+		tmp = tmp->next;
+	}
+	return (0);
+}
+
+void	ft_set_redirect_infile(t_token *redir_token, t_redirect *redirections)
+{
+	if (redirections->infile)
+		free(redirections->infile);
+	if (redirections->delimiter)
+	{
+		free(redirections->delimiter);
+		redirections->delimiter = NULL;
+	}
+	redirections->heredoc = false;
+	redirections->infile = ft_strdup(redir_token->next->content);
+}
+
+void	ft_set_redirect_heredoc(t_token *redir_token, t_redirect *redirections)
+{
+	if (redirections->delimiter)
+		free(redirections->delimiter);
+	if (redirections->infile)
+	{
+		free(redirections->infile);
+		redirections->infile = NULL;
+	}
+	redirections->heredoc = true;
+	redirections->delimiter = ft_strdup(redir_token->next->content);
+}
+
+void	ft_set_redirect_outfile(t_token *redir_token, t_redirect *redirections)
+{
+	if (redirections->outfile)
+		free(redirections->outfile);
+	redirections->append = false;
+	redirections->outfile = ft_strdup(redir_token->next->content);
+}
+
+void	ft_set_redirect_append(t_token *redir_token, t_redirect *redirections)
+{
+	if (redirections->outfile)
+				free(redirections->outfile);
+	redirections->append = true;
+	redirections->outfile = ft_strdup(redir_token->next->content);
+}
