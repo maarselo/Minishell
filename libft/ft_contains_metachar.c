424cf4e5475fc2702c763480790420701294fea8
Francesc Banzo Saavedra <fbanzo-s@car8s4.42barcelona.com>
2025-10-04 16:40:49 +0200

    fixed norminette

--- libft/ft_contains_metachar.c
+++ libft/ft_contains_metachar.c
@@ -6,7 +6,7 @@
 /*   By: fbanzo-s <fbanzo-s@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 23:08:15 by fbanzo-s          #+#    #+#             */
-/*   Updated: 2025/01/10 23:08:15 by fbanzo-s         ###   ########.fr       */
+/*   Updated: 2025/10/04 16:36:07 by fbanzo-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 int	ft_contains_metachar(char *str)
 {
 	int	i;
-
 
 	i = 0;
 	while (str[i])
