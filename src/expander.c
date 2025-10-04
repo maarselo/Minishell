bc6b3aede83f1af27b367f2e9b7a48676b9cd1bc
Francesc Banzo Saavedra <fbanzo-s@car8s4.42barcelona.com>
2025-10-04 17:57:58 +0200

    added env list

--- src/expander.c
+++ src/expander.c
@@ -6,13 +6,13 @@
 /*   By: fbanzo-s <fbanzo-s@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 04:07:31 by fbanzo-s          #+#    #+#             */
-/*   Updated: 2025/09/23 17:05:47 by fbanzo-s         ###   ########.fr       */
+/*   Updated: 2025/10/04 17:13:55 by fbanzo-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	*ft_expand_var(char *str, int *i)
+char	*ft_expand_var(char *str, int *i, t_env *env_list)
 {
 	char	*value;
 	char	*var;
@@ -28,13 +28,13 @@ char	*ft_expand_var(char *str, int *i)
 	while (str[*i] && (ft_isalnum(str[*i]) || str[*i] == '_'))
 		(*i)++;
 	var = ft_substr(str, start, *i - start);
-	value = getenv(var);
+	value = ft_get_env_value(var, env_list);
 	if (value)
 		return (ft_strdup(value));
 	return (ft_strdup(""));
 }
 
-char	*ft_expand_variables(char *str)
+char	*ft_expand_variables(char *str, t_env *env_list)
 {
 	int		i;
 	char	*result;
@@ -43,12 +43,12 @@ char	*ft_expand_variables(char *str)
 	i = 0;
 	while (str[i])
 	{
-		result = ft_handle_expansion(result, str, &i);
+		result = ft_handle_expansion(result, str, &i, env_list);
 	}
 	return (result);
 }
 
-char	*ft_expand_var_no_quotes(char *str)
+char	*ft_expand_var_no_quotes(char *str, t_env *env_list)
 {
 	int		i;
 	char	*result;
@@ -57,19 +57,19 @@ char	*ft_expand_var_no_quotes(char *str)
 	i = 0;
 	while (str[i])
 	{
-		if (str[i] == '~' && (str[i + 1] == '\0'
+		if (i == 0 && str[i] == '~' && (str[i + 1] == '\0'
 				|| str[i + 1] == '/' || ft_isspace(str[i + 1])))
 		{
-			result = ft_expand_tilde(result);
+			result = ft_expand_tilde(result, env_list);
 			i++;
 			continue ;
 		}
-		result = ft_handle_expansion(result, str, &i);
+		result = ft_handle_expansion(result, str, &i, env_list);
 	}
 	return (result);
 }
 
-char	*ft_execute_expander(char *str)
+char	*ft_execute_expander(char *str, t_env *env_list)
 {
 	int	l;
 
@@ -79,38 +79,30 @@ char	*ft_execute_expander(char *str)
 	if (l >= 2 && str[0] == '\'' && str[l - 1] == '\'')
 		return (ft_strndup(str + 1, l - 2));
 	if (l >= 2 && str[0] == '"' && str[l - 1] == '"')
-		return (ft_expand_variables(ft_strndup(str + 1, l - 2)));
-	return (ft_expand_var_no_quotes(str));
+		return (ft_expand_variables(ft_strndup(str + 1, l - 2), env_list));
+	return (ft_expand_var_no_quotes(str, env_list));
 }
 
-void	ft_expand(t_command *cmd)
+void	ft_expand(t_command *cmd, t_env *env_list)
 {
 	int		i;
 	char	*expanded;
 	bool	quotes;
-	char	**wc_expanded;
-	char	**temp_cmd;
 
-	quotes = false;
 	while (cmd)
 	{
 		i = 0;
+		quotes = false;
 		while (cmd->command && cmd->command[i])
 		{
-			if (ft_strchr(cmd->command[i], '\'') || ft_strchr(cmd->command[i], '"'))
+			if (ft_strchr(cmd->command[i], '\'')
+				|| ft_strchr(cmd->command[i], '"'))
 				quotes = true;
-			expanded = ft_execute_expander(cmd->command[i]);
+			expanded = ft_execute_expander(cmd->command[i], env_list);
 			free(cmd->command[i]);
 			cmd->command[i] = expanded;
 			if (quotes == false && ft_strchr(cmd->command[i], '*'))
-			{
-				wc_expanded = ft_expand_wildcard(cmd->command[i]);
-				temp_cmd = cmd->command;
-				cmd->command = ft_join_wildcards(temp_cmd, i, wc_expanded);
-				ft_free_cmd(temp_cmd);
-				i += ft_array_len(wc_expanded);
-				ft_free_cmd(wc_expanded);
-			}
+				ft_execute_wildcards(cmd, &i);
 			else
 				i++;
 		}
