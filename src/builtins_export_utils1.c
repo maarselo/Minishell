72e1a893f5b5cf847d53d7e77c009289c86b842e
maarselo <marce@decaos.com>
2025-10-28 23:14:46 +0100

    fix: remve unncesary function because be do in parser

--- src/builtins_export_utils1.c
+++ src/builtins_export_utils1.c
@@ -12,45 +12,9 @@
 
 #include "minishell.h"
 
-static int	ft_strlen_without_quotes(char *str)
-{
-	int	i;
-	int	total;
-
-	i = -1;
-	total = 0;
-	while (str[++i])
-	{
-		if (str[i] == '\'' || str[i] == '\"')
-			continue ;
-		total++;
-	}
-	return (total);
-}
-
-static char	*ft_remove_quotes(char *str)
-{
-	int		index;
-	int		new_index;
-	char	*new_str;
-
-	index = -1;
-	new_index = 0;
-	new_str = ft_calloc(1, ft_strlen_without_quotes(str) + 1);
-	while (str[++index])
-	{
-		if (str[index] == '\'' || str[index] == '\"')
-			continue ;
-		new_str[new_index] = str[index];
-		new_index++;
-	}
-	return (new_str);
-}
-
 char	*ft_split_name_var(char *str)
 {
 	int		i;
-	char	*tmp;
 	char	*var_name;
 
 	i = 0;
@@ -61,11 +25,7 @@ char	*ft_split_name_var(char *str)
 		i++;
 	}
 	var_name = ft_strndup(str, i);
-	if (!ft_strchr(var_name, '\'') && !ft_strchr(str, '\"'))
-		return (var_name);
-	tmp = var_name;
-	var_name = ft_remove_quotes(var_name);
-	return (free(tmp), var_name);
+	return (var_name);
 }
 
 char	*ft_split_value_var(char *str)
