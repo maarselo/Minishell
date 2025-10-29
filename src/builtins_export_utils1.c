b655fd950ddfaccfe569fe54b821dcd6e1367894
maarselo <marce@decaos.com>
2025-10-29 23:31:32 +0100

    fix: to compile without unncearry functions

--- src/builtins_export_utils1.c
+++ src/builtins_export_utils1.c
@@ -15,7 +15,6 @@
 char	*ft_split_name_var(char *str)
 {
 	int		i;
-	char	*var_name;
 
 	i = 0;
 	while (str[i])
@@ -24,8 +23,7 @@ char	*ft_split_name_var(char *str)
 			break ;
 		i++;
 	}
-	var_name = ft_strndup(str, i);
-	return (var_name);
+	return (ft_strndup(str, i));
 }
 
 char	*ft_split_value_var(char *str)
@@ -33,8 +31,6 @@ char	*ft_split_value_var(char *str)
 	int		index;
 	int		start;
 	int		len;
-	char	*tmp;
-	char	*value;
 
 	index = 0;
 	start = 0;
@@ -51,12 +47,7 @@ char	*ft_split_value_var(char *str)
 		index++;
 		len++;
 	}
-	value = ft_substr(str, start + 1, len);
-	if (!ft_strchr(value, '\'') && !ft_strchr(value, '\"'))
-		return (value);
-	tmp = value;
-	value = ft_remove_quotes(value);
-	return (free(tmp), value);
+	return (ft_substr(str, start + 1, len));
 }
 
 void	ft_replace_env_var(t_env *env_list, char *name_var, char *new_val)
