fc57b21318044ab994481200e2f8435f392e4829
maarselo <marce@decaos.com>
2025-11-04 19:11:35 +0100

    feat:add utils signals

---
+++ src/signals_utils.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signals_utils.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbanzo-s <fbanzo-s@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/30 18:24:52 by fbanzo-s          #+#    #+#             */
+/*   Updated: 2025/10/06 20:48:19 by fbanzo-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	ft_sigint_handler_prompt_mode(int signal)
+{
+	(void)signal;
+	ft_set_global_exit_status(T_SIGINT);
+	write(STDOUT_FILENO, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
+
+void	ft_sigint_hadler_heredoc_mode(int signal)
+{
+	(void)signal;
+	ft_set_global_exit_status(T_SIGINT);
+	ft_set_global_heredoc_status(1);
+}
